a5ba796101eb2f7e5f877fd0b664d8c1b6ec3c26
Roberto Salomón Vázquez Salazar <vqz.salomon@gmail.com>
2025-09-19 05:06:41 +0000

    Cambia el texto de la 'card' ediciones a Ediciones hechas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,7 +115,7 @@ export default function DashboardPage() {
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">
-                Ediciones pendientes
+                Ediciones hechas
               </CardTitle>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
