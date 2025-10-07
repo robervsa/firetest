055940db12c68d2aea43e26236338f7a5ec81b98
Roberto Salomón Vázquez Salazar <vqz.salomon@gmail.com>
2025-10-07 02:59:19 +0000

    Del cambio anterior recupera el menu de hamburgesa

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -57,7 +57,7 @@ export default function Header() {
   
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-50">
-      <nav className="flex-col gap-6 text-lg font-medium flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           href="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
@@ -80,6 +80,39 @@ export default function Header() {
           Mis Gastos
         </Link>
       </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="shrink-0 md:hidden"
+          >
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link
+              href="/"
+              className="flex items-center gap-2 text-lg font-semibold"
+            >
+              <Package2 className="h-6 w-6 text-primary" />
+              <span className="sr-only">GastoControl</span>
+            </Link>
+            {role === 'admin' && (
+              <Link href="/" className="text-muted-foreground hover:text-foreground">
+                <LayoutDashboard className="h-5 w-5 inline-block mr-2" />
+                Dashboard
+              </Link>
+            )}
+             <Link href="/my-expenses" className="text-muted-foreground hover:text-foreground">
+                <Briefcase className="h-5 w-5 inline-block mr-2" />
+                Mis Gastos
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
         <div className="ml-auto flex-1 sm:flex-initial">
           <Link href="/add-expense">
